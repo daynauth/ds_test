aad51164ef411351e84d0c679bab4f553f915249
Roland Daynauth <daynauth@gmail.com>
2023-08-17 12:39:31 -0400

    update run

--- run_mpi_test.sh
+++ run_mpi_test.sh
@@ -4,4 +4,4 @@ then
     container_name="node1"
 fi
 
-docker run --rm --gpus all -it -v $PWD/mpi_test:/home/deepspeed/mpi_test --name $container_name --network deepspeed mpi_test
+docker run --rm --gpus all -it -v $PWD/mpi_test:/home/deepspeed/mpi_test --name $container_name --network deepspeed daynauth/mpi_test:latest
