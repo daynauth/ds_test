9d75eff2da996ad7c1acc4b6611d2692420c433b
Roland Daynauth <daynauth@gmail.com>
2023-08-17 22:33:27 -0400

    testing container

--- run_mpi_test.sh
+++ run_mpi_test.sh
@@ -4,4 +4,5 @@ then
     container_name="node1"
 fi
 
-docker run --rm --gpus all -it -v $PWD/mpi_test:/home/deepspeed/mpi_test --name $container_name --network deepspeed -p 2020:20 daynauth/mpi_test:latest
+docker run --rm --gpus all -it -v $PWD/mpi_test:/home/deepspeed/mpi_test --name $container_name -p 2020:20 daynauth/mpi_test:latest
+#docker run --rm --gpus all -it -v $PWD/mpi_test:/home/deepspeed/mpi_test --name $container_name --network deepspeed -p 2020:20 daynauth/mpi_test:latest
