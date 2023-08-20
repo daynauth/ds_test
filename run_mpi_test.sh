33efb8e7939985356407fab1002650a23f8740ea
Roland Daynauth <daynauth@gmail.com>
2023-08-19 21:26:46 -0400

    slight adjustment

--- run_mpi_test.sh
+++ run_mpi_test.sh
@@ -1,7 +1,15 @@
 container_name=$1
+key_scan=""
+
 if [ -z "$1" ]
 then
     container_name="node1"
 fi
+
+#if container_name is node1
+# if [ "$container_name" = "node1" ]
+# then
+#     key_scan="ssh-keyscan -H node2 >> ~/.ssh/known_hosts"
+# fi
 
 docker run --rm --gpus all -it -v $PWD/mpi_test:/home/deepspeed/mpi_test --name $container_name --network deepspeed -p 2020:22 daynauth/mpi_test:latest
