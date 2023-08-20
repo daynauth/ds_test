ecdb90ed5e666563f713fd8cdf7633ab467535bb
Roland Daynauth <daynauth@gmail.com>
2023-08-20 18:49:08 -0400

    updated infernce

--- run_inference.sh
+++ run_inference.sh
@@ -1,3 +1,10 @@
+container_name=$1
+
 benchmark=$PWD/benchmark
 
-docker run --rm --gpus all -it -v ${benchmark}:/home/deepspeed/benchmark daynauth/deepspeed
+if [ -z "$1" ]
+then
+    container_name="node1"
+fi
+
+docker run --rm --gpus all -it -v ${benchmark}:/home/deepspeed/benchmark --name $container_name daynauth/deepspeed
